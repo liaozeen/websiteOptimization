d5724d88907f648dad44ef54611ffe5be6aeaa44
liaozeen <liaogogogo@qq.com>
2017-06-22 08:28:24 +0800

    修改mian.js

--- views/js/main.js
+++ views/js/main.js
@@ -516,7 +516,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 31; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
